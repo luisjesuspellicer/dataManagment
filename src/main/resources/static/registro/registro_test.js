69b242aa14a69d00eb2bd29ff6edb9d26dcc8a58
luisjesuspellicer <luis_jesus123@hotmail.com>
2016-12-04 19:49:18 +0100

    Data Managment Base 1.0.0

---
+++ src/main/resources/static/registro/registro_test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('frontEndApp.registro module', function() {
+
+  beforeEach(module('frontEndApp.registro'));
+
+  describe('registro controller', function(){
+
+    it('should ....', inject(function($controller) {
+      //spec body
+      var registroCtrl = $controller('registroCtrl');
+      expect(registroCtrl).toBeDefined();
+    }));
+
+  });
+});
---
+++ src/main/resources/static/testCognitivoResultado/testCognitivoResultado_test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('frontEndApp.testCognitivo module', function() {
+
+  beforeEach(module('frontEndApp.testCognitivo'));
+
+  describe('registro controller', function(){
+
+    it('should ....', inject(function($controller) {
+      //spec body
+      var testCognitivoCtrl = $controller('testCognitivoCtrl');
+      expect(testCognitivoCtrl).toBeDefined();
+    }));
+
+  });
+});
